14ef62e6f213061aeb92f96c81326e843a1bc1cf
myko <awesomeme155@gmail.com>
2021-05-19 23:29:12 +0300

    feat: add post http method to create post

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+
+		return
+	}
 	w.Write([]byte("Hello from snippet box...."))
 }
 
@@ -14,11 +19,19 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed!", 405)
+	
+		return
+	}
+
 	w.Write([]byte("Create a new snippet...."))
 }
 
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
