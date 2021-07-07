46865d57cb0e7bcf5b91b7fb96184d552458240e
myko <awesomeme155@gmail.com>
2021-07-07 16:36:48 +0300

    chore: add configuration and improve error handling - add command-line env variable flags - add custom errLog and infoLog error instances - pass the custom error instances to handles using a custom Application type

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *Application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 
@@ -23,7 +22,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		app.errLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 
 		return
@@ -31,12 +30,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.errLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *Application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -47,7 +46,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app Application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method not allowed!", 405)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,24 +1,47 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type Application struct {
+	infoLog *log.Logger
+	errLog *log.Logger
+}
+
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP socket address env variable")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &Application{
+		infoLog: infoLog,
+		errLog: errLog,
+	}
+
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 
-	log.Println("Starting server on :4000")
+	infoLog.Printf("Starting server on %s", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	server := &http.Server{
+		Addr: *addr,
+		ErrorLog: errLog,
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
 
-	log.Fatal(err)
+	errLog.Fatal(err)
 }
