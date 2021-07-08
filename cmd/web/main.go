e0690da3d1f10a196333d39e984edbc2a80ee1bf
myko <awesomeme155@gmail.com>
2021-07-08 17:22:01 +0300

    ft-add-persistance-layer - configure a mysql connection interface

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -12,12 +15,33 @@ type application struct {
 	errLog *log.Logger
 }
 
+func openDBConnection(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP socket address env variable")
+	dsn := flag.String("dsn", "webu:webu@/snippet_box?parseTime=true", "MysQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDBConnection(*dsn)
+	if err != nil {
+		errLog.Fatal(err)
+	}
+	// only necessary if the application a graceful shutdown mechanism
+	defer db.Close()
 
 	app := &application{
 		infoLog: infoLog,
@@ -31,7 +55,7 @@ func main() {
 		ErrorLog: errLog,
 		Handler: app.routes(),
 	}
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 
 	errLog.Fatal(err)
 }
