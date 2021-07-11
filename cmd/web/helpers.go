b4ede4fd260f557024640d392b629d4cfa692ec6
myko <awesomeme155@gmail.com>
2021-07-12 02:45:59 +0300

    feat: pass common dynamic template data
    - add current_year field to the template_data struct
    - add add_default_template_data helper method to application
    - call add_default_template_data helper in the render helper method to pass the template_data struct with the current_year value injected

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
@@ -23,6 +24,15 @@ func (app *application) notFoundError(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
 
+func (app *application) addDefaultTemplateData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+
+	return td
+}
+
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
 	templateSet, ok := app.templateCache[name]
 	if !ok {
@@ -35,8 +45,8 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 	// http.ResponseWriter. If there's an error, call our serverError helper and then
 	// return.
 	buf := new(bytes.Buffer)
-	// excute the template set passing in dynamic data
-	err := templateSet.Execute(buf, td)
+	// excute the template set passing in dynamic data with the current year injected
+	err := templateSet.Execute(buf, app.addDefaultTemplateData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 
