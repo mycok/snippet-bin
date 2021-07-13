baaef234e2c71da87ebf98d838f10df030dd9428
myko <awesomeme155@gmail.com>
2021-07-13 11:37:02 +0300

    feat: add restful routing - add chi router library - update routes module file to user chi router as the app server_mux - update home route handler to user request params instead of query param - add a create_snippet_form handler

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -8,20 +8,20 @@ import (
 	"time"
 )
 
-func (app *application) serverError(w http.ResponseWriter, err error) {
+func (app *application) serverError(wr http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errLog.Output(2, trace)
 
-	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	http.Error(wr, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
-func (app *application) clientError(w http.ResponseWriter, status int) {
-	http.Error(w, http.StatusText(status), status)
+func (app *application) clientError(wr http.ResponseWriter, status int) {
+	http.Error(wr, http.StatusText(status), status)
 }
 
 // wrapper around clientError for notFoundError
-func (app *application) notFoundError(w http.ResponseWriter) {
-	app.clientError(w, http.StatusNotFound)
+func (app *application) notFoundError(wr http.ResponseWriter) {
+	app.clientError(wr, http.StatusNotFound)
 }
 
 func (app *application) addDefaultTemplateData(td *templateData, r *http.Request) *templateData {
@@ -33,10 +33,10 @@ func (app *application) addDefaultTemplateData(td *templateData, r *http.Request
 	return td
 }
 
-func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
+func (app *application) render(wr http.ResponseWriter, r *http.Request, name string, td *templateData) {
 	templateSet, ok := app.templateCache[name]
 	if !ok {
-		app.serverError(w, fmt.Errorf("The template %s does not exit", name))
+		app.serverError(wr, fmt.Errorf("The template %s does not exit", name))
 
 		return
 	}
@@ -48,10 +48,10 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 	// excute the template set passing in dynamic data with the current year injected
 	err := templateSet.Execute(buf, app.addDefaultTemplateData(td, r))
 	if err != nil {
-		app.serverError(w, err)
+		app.serverError(wr, err)
 
 		return
 	}
 	// write the contents of the buffer to http.ResponseWriter
-	buf.WriteTo(w)
+	buf.WriteTo(wr)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/mycok/snippet-bin
 go 1.16
 
 require (
+	github.com/go-chi/chi/v5 v5.0.3 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/justinas/alice v1.2.0
 )
